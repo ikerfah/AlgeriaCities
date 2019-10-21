cf7997716d02e663713de75f7e5a72cc7d98d7cd
KERFAH Issam <i.kerfah@gmail.com>
2019-10-21 21:43:13 +0100

    Add LatLng class to represent location

---
+++ algeriacities/src/main/java/com/ikerfah/algeriacities/LatLng.kt
@@ -0,0 +1,11 @@
+package com.ikerfah.algeriacities
+
+class LatLng {
+    var latitude: String? = null
+    var longitude: String? = null
+
+    constructor(latitude: String?, longitude: String?) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+}
