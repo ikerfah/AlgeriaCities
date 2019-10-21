e611852f75c77aba18a31791e8ec69dd911eadff
KERFAH Issam <i.kerfah@gmail.com>
2019-10-21 15:34:36 +0100

    Add Commune class sunclass of Zone

---
+++ algeriacities/src/main/java/com/ikerfah/algeriacities/Commune.kt
@@ -0,0 +1,11 @@
+package com.ikerfah.algeriacities
+
+class Commune : Zone {
+
+    constructor(id: Int?, nomFr: String?, nomAr: String?, codePostal: Int?) : super(
+        id,
+        nomFr,
+        nomAr,
+        codePostal
+    )
+}
