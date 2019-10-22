0795127f2667b1b5dd3fd4ea3c2e5b3c8ba6a413
KERFAH Issam <i.kerfah@gmail.com>
2019-10-23 00:39:10 +0100

    Add default base adapter for wilaya and communes

---
+++ algeriacities/src/main/java/com/ikerfah/algeriacities/WilayaAdapter.kt
@@ -0,0 +1,68 @@
+package com.ikerfah.algeriacities
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.appcompat.widget.AppCompatTextView
+import kotlinx.android.synthetic.main.item_text.view.*
+
+class WilayaAdapter<T : Zone> : BaseAdapter {
+
+    companion object {
+        val LANG_AR: String = "arabic"
+        val LANG_FR: String = "french"
+    }
+
+    private var mContext:Context
+    private var mItems: ArrayList<T> = arrayListOf()
+    private var lang: String = LANG_FR
+
+    constructor(context: Context, items: ArrayList<T>, defaultLanguage: String?) : super() {
+        this.mContext = context
+        this.mItems = items
+        this.lang = defaultLanguage?: LANG_FR
+    }
+
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view : View
+        val viewHolder : ViewHolder
+        if (convertView == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.item_text, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        }else{
+            view = convertView
+            viewHolder = convertView.tag as WilayaAdapter<T>.ViewHolder
+        }
+        viewHolder.name.text = if (lang == LANG_FR) mItems[position].nomFr else mItems[position].nomAr
+        return view
+    }
+
+    override fun getItem(p0: Int): Any {
+        return mItems[p0]
+
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return mItems[p0].id.toLong()
+    }
+
+    override fun getCount(): Int {
+        return if (mItems != null) mItems.size else 0
+    }
+
+    private inner class ViewHolder{
+         var name : AppCompatTextView
+
+         constructor(row: View){
+            name = row.text_name
+        }
+
+    }
+
+
+}
