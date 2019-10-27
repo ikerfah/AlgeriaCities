ea5ae3106304ddb8ea6554047645a2dcdfb1e1b5
KERFAH Issam <i.kerfah@gmail.com>
2019-10-27 21:18:36 +0100

    Make wilayas bounds array const

--- algeriacities/src/main/java/com/ikerfah/algeriacities/helper/Validations.kt
+++ algeriacities/src/main/java/com/ikerfah/algeriacities/helper/Validations.kt
@@ -3,8 +3,8 @@ package com.ikerfah.algeriacities.helper
 class Validations {
 
     companion object {
-        private val MIN_WILAYA_ID = 1
-        private val MAX_WILAYA_ID = 48
+        private const val MIN_WILAYA_ID = 1
+        private const val MAX_WILAYA_ID = 48
         internal fun isWilayaIdCorrect(wilayaId: Int): Boolean {
             return wilayaId != null && wilayaId in MIN_WILAYA_ID..MAX_WILAYA_ID
         }
